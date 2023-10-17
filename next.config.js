050d4bea8305cd7c335edef3dcda07c7dd0e29f4
Brian D. Wendt <briandwendt@gmail.com>
2023-10-17 17:03:32 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: [
+      'avatars.githubusercontent.com',
+      'avatar.vercel.sh',
+      'tailwindui.com',
+      'images.unsplash.com'
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
